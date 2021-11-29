3bea58264e0cdf97213fad1bf35525f5bb54bb2b
rajasurender11 <rajasurender11@gmail.com>
2021-11-29 08:58:15 +0530

    Dataframe operations

--- src/main/scala/com/sample/main/SampleMain.scala
+++ src/main/scala/com/sample/main/SampleMain.scala
@@ -1,5 +1,7 @@
 package com.sample.main
 
+import org.apache.spark.sql.functions.lit
+import org.apache.spark.sql.functions.{col, desc, sum}
 import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.apache.spark.sql.types.{StringType, StructField, StructType}
 import org.slf4j.LoggerFactory
@@ -7,6 +9,7 @@ import org.slf4j.LoggerFactory
 object SampleMain {
 
   def main(args: Array[String]): Unit = {
+    import spark.implicits._
     val log = LoggerFactory.getLogger(this.getClass)
    val spark = SparkSession.builder
       .appName("SparkExample")
@@ -30,9 +33,30 @@ val rowRDD = rdd.map(rec => rec.split(",")).map(arr => org.apache.spark.sql.Row(
 val df = spark.createDataFrame(rowRDD,schema)
     df.printSchema()
     //df.show(100,false)
+    df.show(11)
     val outputLoc = "/user/training/surender_hadoop/output_files/accounts_profile"
     //df.coalesce(1).write.format(format).mode(mode).save(hdfsOutputLoc)
+
+    val selectedDF = df.select("bank_name","account_no", "gender")
+
+    val sDF = df.select("bank_name","account_no")
+    val df1 = df.drop("bank_name")
+    df.createOrReplaceTempView("t1")
+    val sqlDF = spark.sql(
+      """
+        |select account_no, ph_no, bank_name, 'INDIA' as country from t1 where bank_name = 'HDFC'
+        |""".stripMargin
+    )
+
+    val colsList = List("account_no","ph_no","bank_name")
+  val changedDF = df.withColumn("country",lit("INDIA"))
+    .withColumn("c2",lit("HELLO"))
+    .drop("c2")
+
+    val mappedDF = df.select(colsList.map(m=>col(m)):_*).show()
     df.write.format("parquet").mode(SaveMode.Overwrite).save(outputLoc)
+
+    val renamedDF = df.withColumnRenamed("ph_no", "mobile_no")
 
   }
 
